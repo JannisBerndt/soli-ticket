ea934976ef620213f0a8da30759777f4fe36927b
Tratori <FabianMahling@web.de>
2020-03-19 00:50:20 +0100

    Put example page on "/"

---
+++ solisite/views.py
@@ -0,0 +1,7 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def index(request):
+    return render(request, "index.html")
