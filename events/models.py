e61106868d70341e7219df7960e7bfb3ad3b90a9
JannisBerndt <berndtjannis@gmail.com>
2020-03-19 17:08:59 +0100

    commented out class Buyable and Address

--- events/models.py
+++ events/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 
-class Buyable(models.Model):
-	name = models.CharField(max_length=120)
-	price = models.DecimalField(max_digits=1000, decimal_places=2)
+# class Buyable(models.Model):
+# 	name = models.CharField(max_length=120)
+# 	price = models.DecimalField(max_digits=1000, decimal_places=2)
 
-class Address(models.Model):
-	country = models.CharField(max_length=120)
-	city = models.CharField(max_length=120)
-	street = models.CharField(max_length=120)
-	house_number = models.CharField(max_length=120)
-	post_code = models.DecimalField(max_digits=5, decimal_places=0)
+# class Address(models.Model):
+# 	country = models.CharField(max_length=120)
+# 	city = models.CharField(max_length=120)
+# 	street = models.CharField(max_length=120)
+# 	house_number = models.CharField(max_length=120)
+# 	post_code = models.DecimalField(max_digits=5, decimal_places=0)
 
 class Event(models.Model):
 	name = models.CharField(max_length=120)
@@ -20,7 +20,6 @@ class Event(models.Model):
 	address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
 	# buyable = models.ManyToManyField(Buyable, null=True, blank=True)
 	# from .Address import country, city, street, house_number, post_code
-	
 	country = models.CharField(max_length=120)
 	city = models.CharField(max_length=120)
 	street = models.CharField(max_length=120)
