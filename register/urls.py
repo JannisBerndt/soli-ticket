7b614b32a7f9e8be3dae20bd84a1300eebabd7e5
Nils <nils.koenig01@gmail.com>
2020-03-19 14:52:22 +0100

    Modified teamplates to display users from database

--- register/urls.py
+++ register/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register_start),
+    path('', views.user_list),
+    path('register/', views.register_start),
 ]
--- register/views.py
+++ register/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 from .forms import RegisterForm
+
+def user_list(request):
+    users = User.objects.all()
+    return render(request, 'register/user_list.html', {'users': users})
 
 def register_start(request):
     if request.method == "POST":
