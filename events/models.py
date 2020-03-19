e1fac1425db8ee218428265f558767cd5a8d1a17
JannisBerndt <berndtjannis@gmail.com>
2020-03-19 17:08:20 +0100

    added migrations

--- events/models.py
+++ events/models.py
@@ -18,4 +18,12 @@ class Event(models.Model):
 	description = models.TextField(null=True, blank=True)
 	date = models.DateTimeField(null=True, blank=True)
 	address = models.ForeignKey(Address, on_delete=models.SET_NULL, null=True, blank=True)
-	buyable = models.ManyToManyField(Buyable, null=True, blank=True)
+	# buyable = models.ManyToManyField(Buyable, null=True, blank=True)
+	# from .Address import country, city, street, house_number, post_code
+	
+	country = models.CharField(max_length=120)
+	city = models.CharField(max_length=120)
+	street = models.CharField(max_length=120)
+	house_number = models.CharField(max_length=120)
+	post_code = models.DecimalField(max_digits=5, decimal_places=0)
+
