1ddd222d28f9e29e9fc9031704069fdf82340021
Nils <nils.koenig01@gmail.com>
2020-03-22 00:33:15 +0100

    Added Login functionality

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_page, name = 'login'),
+    path('logout/', views.logout_user, name = 'logout'),
+
     path('profile/<str:pk>/', views.profile, name='profile'),
     path('register/', views.accounts.as_view(), name='register'),
     path('error/', views.error, name='register_error')
--- accounts/views.py
+++ accounts/views.py
@@ -4,8 +4,37 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('profile', user.username)
+            else:
+                messages.info(request, 'Username or Password is incorrect')
+
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+    return render(request, 'accounts/login.html')
+
+
+@login_required(login_url='login')
 def profile(request, pk):
     organiser = Organiser.objects.get(username=pk)
     context = {'organiser': organiser}
