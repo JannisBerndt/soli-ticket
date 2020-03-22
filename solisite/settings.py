0f962b5f700e2b9439eff125c08396d255634bfe
JannisBerndt <berndtjannis@gmail.com>
2020-03-22 14:00:00 +0100

    updated base template reference in events

--- solisite/settings.py
+++ solisite/settings.py
@@ -63,7 +63,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, "solisite/templates"),
-                 os.path.join(BASE_DIR, "accounts/templates"),],
+                 os.path.join(BASE_DIR, "accounts/templates"),
+				 os.path.join(BASE_DIR, "templates"),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
