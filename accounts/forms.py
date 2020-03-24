bbed6719f3d47cf0ad355f3a4297a9c96637408a
JannisBerndt <berndtjannis@gmail.com>
2020-03-24 17:48:35 +0100

    fixed mobile event detail page

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,11 +4,11 @@ from django.forms import inlineformset_factory
 from events.models import Buyable
 
 class OrderForm(forms.ModelForm):
-	article = forms.CharField(disabled=True)
+	# article = forms.CharField(disabled=True)
 	amount = forms.IntegerField(label='Anzahl', min_value=0, initial=0, widget=forms.NumberInput(attrs={'class': 'text-field-2 w-input', 'id': 'field-3'}))
 	class Meta:
 		model = Order
 		fields = [
 			'amount',
-			'price',
+			# 'price',
 		]
