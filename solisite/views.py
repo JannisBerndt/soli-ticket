5a59088b5af7709e45c2d839c96580869c786178
JannisBerndt <berndtjannis@gmail.com>
2020-03-25 11:56:16 +0100

    solisite views.py now provides organiser_user in every currently used view

--- solisite/views.py
+++ solisite/views.py
@@ -1,31 +1,56 @@
 from django.shortcuts import render, redirect
 
 def landingpage_view(request):
+    try:
+        organiser_user = Organiser.objects.get(username = request.user.username)
+    except:
+        organiser_user = None
     context = {
         'authenticated': request.user.is_authenticated,
+        'organiser_user': organiser_user,
     }
     return render(request, 'solisite/landingpage.html', context)
 
 def about_view(request):
-	context = {
-        'authenticated': request.user.is_authenticated,
-	}
-	return render(request, 'solisite/about.html', context)
-
-def imprint_view(request):
-	context = {
-        'authenticated': request.user.is_authenticated,
-	}
-	return render(request, 'solisite/imprint.html', context)
-
-def privacy_policy_view(request):
-	context = {
-        'authenticated': request.user.is_authenticated,
-	}
-	return render(request, 'solisite/privacy_policy.html', context)
-
-def blog_view(request):
+    try:
+        organiser_user = Organiser.objects.get(username = request.user.username)
+    except:
+        organiser_user = None
     context = {
         'authenticated': request.user.is_authenticated,
+        'organiser_user': organiser_user,
+    }
+    return render(request, 'solisite/about.html', context)
+
+def imprint_view(request):
+    try:
+        organiser_user = Organiser.objects.get(username = request.user.username)
+    except:
+        organiser_user = None
+    context = {
+        'authenticated': request.user.is_authenticated,
+        'organiser_user': organiser_user,
+    }
+    return render(request, 'solisite/imprint.html', context)
+
+def privacy_policy_view(request):
+    try:
+        organiser_user = Organiser.objects.get(username = request.user.username)
+    except:
+        organiser_user = None
+    context = {
+        'authenticated': request.user.is_authenticated,
+        'organiser_user': organiser_user,
+    }
+    return render(request, 'solisite/privacy_policy.html', context)
+
+def blog_view(request):
+    try:
+        organiser_user = Organiser.objects.get(username = request.user.username)
+    except:
+        organiser_user = None
+    context = {
+        'authenticated': request.user.is_authenticated,
+        'organiser_user': organiser_user,
     }
     return render(request, 'solisite/blog.html', context)
