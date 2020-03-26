59ff78b586e229b00a5040a7c00495062f316ec5
Nils <nils.koenig01@gmail.com>
2020-03-26 11:30:10 +0100

    Email Functionality working

--- events/views.py
+++ events/views.py
@@ -18,12 +18,13 @@ def event_detail_view(request, id):
 	location = event.location
 	OrderFormSet = inlineformset_factory(Customer, Order, form=OrderForm, fields=['amount',], extra=buyables.count())
 	order_formset = OrderFormSet(queryset=Order.objects.none())
+
 	if request.method == 'POST':
 		try:
 			customer = Customer.objects.get(username=request.user.username)
 		except:
 			customer = None
-		# Order.objects.all().delete()	dont delete every single order
+
 		order_formset = OrderFormSet(request.POST, instance=customer)
 		if order_formset.is_valid():
 			i=0
@@ -34,22 +35,13 @@ def event_detail_view(request, id):
 				if order.amount:
 					order.article = buyables[i]
 					order.price = buyables[i].price * order.amount
-					#print(order.price)
-					#print(order.article)
-					#if customer.customer_set.first():
-					#	print("Gibts schon")
-					#	Order.objects.get(article=customer.customer_set.first().article).amount = Order.objects.get(article=customer.customer_set.first().article).amount + order.amount
-					#	customer.customer_set.first().price += order.price
-					#	print(customer.customer_set.first().price + order.price)
-					#	Order.objects.get(article=customer.customer_set.first().article).save()
-					#	print(customer.customer_set.first().amount)
-					#else:
 					order.customer = customer
+					order.customer_mail = request.POST.get('field-4')
 					order.save()
 					sum += order.price
 					orders.append(order)
 				i += 1
-			print(orders)
+
 			organiser = event.creator
 			context = {
 				'sum': sum,
@@ -71,14 +63,11 @@ def event_detail_view(request, id):
 			Viele Grüße und vielen, vielen Dank von ' + organiser.organisation_name + ' und dem Team von www.soli-ticket.de ! \n\
 			P.S. Sie wollen kostenfrei noch mehr beitragen? Teilen Sie www.soli-ticket.de und alle interessanten Veranstaltungen mit Ihren Kontakten!'
 
-			print(message)
-			
-			#send_mail(subject, message, settings.EMAIL_HOST_USER, ['nils.koenig01@hotmail.de'])
+			send_mail(subject, message, settings.EMAIL_HOST_USER, [request.POST.get('field-4')])
 
 			return render(request, "event/event_donate.html", context)
-			# return redirect('events:checkout', event.id)
+
 	formset = zip(buyables, order_formset)
-	print(order_formset)
 	context = {
 		'event': event,
 		'buyables': buyables,
