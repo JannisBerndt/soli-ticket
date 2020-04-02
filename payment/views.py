c5017cd6b3a9156fcf1f5ace869a826b890f8042
Paul Rößler <roessler.paul@web.de>
2020-04-02 15:01:48 +0200

    Notify-url updated

--- payment/views.py
+++ payment/views.py
@@ -19,7 +19,7 @@ def payment_process(request):
 		'item_name': 'Spende über Soli-Ticket.de',
 		'invoice': str(order_id),
 		'currency_code': 'EUR',
-		'notify_url': 'http://{}{}'.format(host, reverse('paypal-ipn')),
+		'notify_url': 'http://solitest.pythonanywhere.com/payment/notify/',
 		'return_url': 'http://127.0.0.1:8000/'.format(host, reverse('payment:done')),
 		'cancel_return': 'http://{}{}'.format(host, reverse('payment:canceled')),
 	}
