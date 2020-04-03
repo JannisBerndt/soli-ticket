44b5335e4eb0d4027afb00cf54cd00669546ed3f
Paul Rößler <roessler.paul@web.de>
2020-04-03 18:03:42 +0200

    PayPal... Orders werden jetzt einzeln hinzugefügt

--- payment/views.py
+++ payment/views.py
@@ -5,13 +5,13 @@ from django.views.decorators.csrf import csrf_exempt
 from decimal import Decimal
 from paypal.standard.forms import PayPalPaymentsForm
 from events.models import Buyable
-from accounts.models import Order
+from accounts.models import Order, UserAddress
+from django.db.models.query import RawQuerySet
 
 # Create your views here.
 
 def payment_process(request):
 	invoiceUID = request.session["invoiceUID"]
-	sum = request.session["sum"]
 	paypal_email = request.session["paypal_email"]
 	
 	if settings.PAYPAL_TEST:
@@ -23,26 +23,57 @@ def payment_process(request):
 	
 	if orders is None:
 		return Exception
-	
-	amount = sum/1.19
-	tax = amount*0.19
 
+	"""
+	sql = 'SELECT au.id, strasse, hnummer, plz, ort FROM accounts_useraddress AS au INNER JOIN auth_user AS auu ON au.id = auu.id INNER JOIN events_buyable AS eb ON eb.creator_id = auu.id INNER JOIN accounts_order AS ao ON eb.id = ao.article_id WHERE ao.invoiceUID = %s'
+	o_adresse = UserAddress.objects.raw(sql, [invoiceUID])[0]
+
+	hnummer = o_adresse.hnummer
+	strasse = o_adresse.strasse
+	ort = o_adresse.ort
+	plz = o_adresse.plz
+	
+	print(hnummer + '\n' + strasse + '\n' + ort + '\n' + plz)
+	"""
+	
 	paypal_dict = {
+		'cmd':'_cart',
+		'upload':1,
 		'business': paypal_email,
-		'amount': '%.2f' % amount,
-		'tax': '%.2f' % tax,
 		'invoice': str(invoiceUID),
 		'currency_code': 'EUR',
 		'notify_url': '{host_base_url}payment/notify/'.format(host_base_url = host),
 		'return_url': '{host_base_url}payment/done/'.format(host_base_url = host),
 		'cancel_return': '{host_base_url}payment/canceled/'.format(host_base_url = host),
+		'submit':'PayPal',
 	}
 
-	i = 1
-	
-	paypal_dict['cmd'] = '_cart'
-	paypal_dict['upload'] = 1
 
+	i = 1
+	for order in orders:
+		itemNamePlatzhalter = 'item_name_'+str(i)
+		steuerWertPlatzhalter = 'tax_'+str(i)
+		itemAnzahlPlatzhalter = 'quantity_'+str(i)
+		itemPreisPlatzhalter = 'amount_'+str(i)
+
+		article_id = order.article_id
+		article = Buyable.objects.get(id = article_id)
+
+		tax_rate = article.tax_rate
+
+		itemName = article.buyable_name
+		itemPreis = article.price/(1+tax_rate)
+		itemSteuer = itemPreis * tax_rate
+		itemAnzahl = order.amount
+
+		paypal_dict[itemNamePlatzhalter] = itemName
+		paypal_dict[steuerWertPlatzhalter] = itemSteuer
+		paypal_dict[itemAnzahlPlatzhalter] = itemAnzahl
+		paypal_dict[itemPreisPlatzhalter] = itemPreis
+
+		i+=1
+
+	"""
 	paypal_dict['item_name_1'] = 'Cola'
 	paypal_dict['item_name_2'] = 'Eintrittskarte'
 	paypal_dict['tax_1'] = 0.5
@@ -51,11 +82,7 @@ def payment_process(request):
 	paypal_dict['amount_2'] = 1.00
 	paypal_dict['quantity_1'] = 1
 	paypal_dict['quantity_2'] = 3
-	paypal_dict['submit'] = 'PayPal'
-	paypal_dict['shipping_1'] = 4.00
-	paypal_dict['shipping_2'] = 8.00
 
-	"""
 	for order in orders:
 		
 		platzhalter = 'item_name_'+str(i)
