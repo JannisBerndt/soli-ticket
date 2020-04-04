697da3170ae216e5af83fff2c74911609bf5bebe
Paul Rößler <roessler.paul@web.de>
2020-04-04 19:16:04 +0200

    IPN - Handling customized... Hoffentlich fliegt uns das nicht um die Ohren

--- payment/urls.py
+++ payment/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 
-from .views import payment_canceled, payment_done, payment_process
+from .views import payment_canceled, payment_done, payment_process, payment_ipn
 
 app_name = "payment"
 urlpatterns = [
 	path('done/', payment_done, name='done'),
 	path('canceled/', payment_canceled, name='canceled'),
 	path('process/', payment_process, name='process'),
-	path('notify/', include('paypal.standard.ipn.urls'))
+	path('notify/', payment_ipn, name='ipn')
 ]
