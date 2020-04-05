0935eae08b1d1efefcb71a009890d30ee9b757c3
Paul Rößler <roessler.paul@web.de>
2020-04-05 09:12:37 +0200

    Hoffentlich final commit zu paypal-dankes-email

--- payment/views.py
+++ payment/views.py
@@ -214,14 +214,12 @@ def payment_ipn(request):
 def sendDankesEmail(ipn_obj):
 	
 	# Orderobjekt für E-Mail Adresse des Käufers. Organisation für Name des Veranstalters.
-	
-	#Order = Order.objects.filter(invoiceUID = ipn_obj.invoice)[0]
-
+	o_Order = Order.objects.filter(invoiceUID = ipn_obj.invoice)[0]
 	o_Organisation = Organiser.objects.get(paypal_email = ipn_obj.receiver_email)
 
-	subject = 'Vielen vielen Dank für Ihre Unterstützung an {orga}'.format(orga = o_Organisation.organisation_name)
-	content = 	'Ihre Unterstützung ist bei Soliticket angekommen!'\
-				'Vielen vielen Dank dafür, dass Sie den Fortbestand unserer Kulturlandschaft aktiv unterstützen. Soliticket und das Team von Soli-Ticket.de danken Ihnen von ganzem Herzen!\n'\
+	subject = 'Vielen vielen Dank für Ihre Unterstützung.'
+	content = 	'Ihre Unterstützung ist bei {Veranstalter} angekommen!'.format(Veranstalter = o_Organisation.organisation_name)+\
+				'Vielen vielen Dank dafür, dass Sie den Fortbestand unserer Kulturlandschaft aktiv unterstützen. {Veranstalter} und das Team von Soli-Ticket.de danken Ihnen von ganzem Herzen!\n'.format(Veranstalter = o_Organisation.organisation_name)+\
 				'Wir, das Team von Soli-Ticket, betreiben diese kostenfreie Plattform als Projekt neben unserem Studium. Falls Sie auch uns eine Kaffee ausgeben möchten oder uns helfen möchten unsere Kosten zu decken, können Sie dies auf der folgenden Seite tun: Link zu unserer Veranstaltung.\n'\
 				'Genau so würde es uns und den vielen Veranstaltern und Kulturstätten helfen, wenn Sie unsere Plattform weiterempfehlen. An Ihre Bekannten, Freunde, Kollegen, aber natürlich auch an andere Kulturstätten, die Ihnen am Herzen liegen.\n'\
 				'Vielen, vielen Dank.\n\n'\
@@ -230,5 +228,5 @@ def sendDankesEmail(ipn_obj):
 	if settings.PAYPAL_TEST:
 		send_mail(subject, content, settings.EMAIL_HOST_USER, ['roessler.paul@web.de'])
 	send_mail(subject, content, settings.EMAIL_HOST_USER, ['roessler.paul@web.de', 'kolzmertz@gmail.com'])
-	#send_mail(subject, content, settings.EMAIL_HOST_USER, [Order.customer_mail])
+	send_mail(subject, content, settings.EMAIL_HOST_USER, [o_Order.customer_mail])
 #endregion
