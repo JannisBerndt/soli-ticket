1fc10bb3aef81599472db300e98675ea23bb57e5
Nils <nils.koenig01@gmail.com>
2020-04-09 17:22:21 +0200

    Modified Organiser model and filter

--- accounts/filters.py
+++ accounts/filters.py
@@ -1,15 +1,11 @@
 import django_filters
 from django_filters import CharFilter
-#from django_filters.groups import CombinedGroup
-
 from .models import Organiser, UserAddress
+
 
 class OrganiserFilter(django_filters.FilterSet):
     organisation_name = CharFilter(field_name='organisation_name', lookup_expr='icontains', label='Name der Organisation')
     city = CharFilter(field_name='user_address__ort', lookup_expr='icontains', label='Ort')
     class Meta:
         model = Organiser
-        fields = []#['organisation_name', 'user_address__ort']
-        #groups = [
-        #    CombinedGroup(filters=['organisation_name', 'user_address__ort'], combine=operator.or_),
-        #]
+        fields = ['organisation_type']
