04dfcb13dda6cd34edd25cb06d1dd6dfcba2bd63
Jannis Berndt <berndtjannis@gmail.com>
2020-04-09 19:19:53 +0200

    updated events models

--- events/models.py
+++ events/models.py
@@ -11,7 +11,7 @@ class Eventlocation(models.Model):
 	createdDateTime = models.DateTimeField(auto_now_add=True)
 	changedDateTime = models.DateTimeField(auto_now=True)
 	location_name = models.CharField(max_length=120)
-	country = models.CharField(max_length=120, null=True, blank=True)
+	country = models.CharField(max_length=120, null=True, blank=True, default="Deutschland")
 	city = models.CharField(max_length=120, null=True, blank=True)
 	street = models.CharField(max_length=120, null=True, blank=True)
 	house_number = models.CharField(max_length=120, null=True, blank=True)
@@ -27,8 +27,8 @@ class Event(models.Model):
 	createdDateTime = models.DateTimeField(auto_now_add=True)
 	changedDateTime = models.DateTimeField(auto_now=True)
 	name = models.CharField(max_length=120)
-	description = models.TextField(null=True, blank=True)
-	date = models.DateField(null=True, blank=True)
+	description = models.TextField()
+	date = models.DateField()
 	time = models.TimeField(null=True, blank=True)
 	location = models.ForeignKey(Eventlocation, on_delete=models.SET_NULL, null=True, related_name='+')
 
@@ -53,8 +53,10 @@ class Buyable(models.Model):
 	changedDateTime = models.DateTimeField(auto_now=True)
 	buyable_name = models.CharField(max_length=120)
 	price = models.DecimalField(max_digits=1000, decimal_places=2, validators=[MinValueValidator(0)])
-	belonging_event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='event_buyable', related_query_name='buyables_set')
-	tax_rate = models.DecimalField(decimal_places = 2, default = 0.19, max_digits = 3, choices=TAX_RATES)
+	belonging_event = models.ForeignKey(Event, on_delete=models.CASCADE,
+										related_name='event_buyable',
+										related_query_name='buyables_set')
+	tax_rate = models.DecimalField(decimal_places = 2, max_digits = 3, choices=TAX_RATES, default = NINETEEN)
 
 	def __str__(self):
 		return self.buyable_name
