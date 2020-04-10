32613f736b885e8f4aa5004611a98d22578531e2
Jannis Berndt <berndtjannis@gmail.com>
2020-04-10 14:00:29 +0200

    password reset flow completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
+from solisite.settings import EMAIL_HOST_USER
 from . import views
 
 app_name='accounts'
@@ -19,9 +20,13 @@ urlpatterns = [
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='register/password_change.html', success_url=reverse_lazy('accounts:password_change_complete')), name="password_change"),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='register/password_change_done.html'), name='password_change_complete'),
 
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='register/request_password_reset.html',
+                                                                 success_url=reverse_lazy('accounts:password_reset_done'),
+                                                                 from_email=EMAIL_HOST_USER,
+                                                                 email_template_name='register/password_reset_email.html',
+                                                                 subject_template_name='register/password_reset_email_subject.txt'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='register/request_password_reset_done.html'), name='password_reset_done'),
 
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='register/password_reset.html', success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='register/password_reset_done.html'), name='password_reset_complete'),
 ]
