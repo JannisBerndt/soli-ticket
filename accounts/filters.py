46a0828abb0c50569ad7a2b09931f67c11cc4b88
Nils <nils.koenig01@gmail.com>
2020-04-10 13:46:41 +0200

    Added template and updated css files

--- accounts/filters.py
+++ accounts/filters.py
@@ -4,8 +4,9 @@ from .models import Organiser, UserAddress
 
 
 class OrganiserFilter(django_filters.FilterSet):
-    organisation_name = CharFilter(field_name='organisation_name', lookup_expr='icontains', label='Name der Organisation')
+    # Maybe add that later:
+    # organisation_name = CharFilter(field_name='organisation_name', lookup_expr='icontains', label='Name der Organisation')
     city = CharFilter(field_name='user_address__ort', lookup_expr='icontains', label='Ort')
     class Meta:
         model = Organiser
-        fields = ['organisation_type']
+        fields = []
