664e63a389ec06692a46126bbcd2e977adb6f522
Jannis Berndt <berndtjannis@gmail.com>
2020-04-11 14:05:44 +0200

    fixed some views

--- solisite/views.py
+++ solisite/views.py
@@ -2,22 +2,22 @@ from django.shortcuts import render, redirect
 from accounts.models import Organiser
 
 def landingpage_view(request):
-    return render(request, 'solisite/landingpage.html', context)
+    return render(request, 'solisite/landingpage.html')
 
 def about_view(request):
-    return render(request, 'solisite/about.html', context)
+    return render(request, 'solisite/about.html')
 
 def imprint_view(request):
-    return render(request, 'solisite/imprint.html', context)
+    return render(request, 'solisite/imprint.html')
 
 def privacy_policy_view(request):
-    return render(request, 'solisite/privacy_policy.html', context)
+    return render(request, 'solisite/privacy_policy.html')
 
 def blog_view(request):
-    return render(request, 'solisite/blog.html', context)
+    return render(request, 'solisite/blog.html')
 
 def faq_view(request):
-    return render(request, 'solisite/faq.html', context)
+    return render(request, 'solisite/faq.html')
 
 def agb_view(request):
-    return render(request, 'solisite/agb.html', context)
+    return render(request, 'solisite/agb.html')
